f9f2f8a354b9c70b211fd117b1062c0407a184af
Антон Аладьин <aladin.anton95@gmail.com>
2024-11-14 17:37:28 +0700

    Updates

---
+++ notes/src/components/PopupComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="isPopupOpen" @click="close" class="popup-wrapper">
+        <div class="popup">
+            <div class="popup-content">
+                <button @click="close" class="button round">
+                    <img src="../assets/images/close.svg" alt="">
+                </button>
+                <slot name="title"></slot>
+                <slot name="content"></slot>
+                <slot name="action"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+    import { ref, computed } from 'vue';
+    import { usePopupStore } from '@/store/popup';
+
+    const popupStore = usePopupStore();
+
+    // console.log(popupStore.getIsPopupOpen.isOpen);
+    
+    const isPopupOpen = false
+
+    
+</script>
---
+++ notes/src/store/popup.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+
+export const usePopupStore = defineStore('popup', {
+  state: () => ({
+    isLoading: false,
+    popup: {
+      isOpen: false,
+      type: null
+    }
+  }),
+  actions: {
+    setIsPopupOpen(type, status) {
+      this.popup.type = type;
+      this.popup.isOpen = status;
+    },
+    setIsLoading(value) {
+      this.isLoading = value;
+    }
+  },
+  getters: {
+    getIsPopupOpen(state) {
+      return state.popup;
+    },
+    getIsLoading(state) {
+      return state.isLoading;
+    }
+  }
+});
